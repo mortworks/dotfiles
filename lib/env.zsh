c4f9474214c86ae3efc629d53b6d07d30a98ab2d
mortworks <pwm@mortmain.uk>
2025-06-01 15:41:30 +0100

    fix: support local Mac dev path in env.zsh

--- lib/env.zsh
+++ lib/env.zsh
@@ -2,19 +2,19 @@
 # 🌍 Detect environment and set project paths
 # ----------------------------------------
 
-# Set EDITOR default if not already defined
 export EDITOR="${EDITOR:-nano}"
 
-# Set PROJECTS path dynamically based on environment
 if [[ "$CODESPACES" == "true" ]]; then
   export PROJECTS="/workspaces"
   export TOOLS_DIR="/workspaces/mortworks/workflow-tools"
+elif [[ -d "$HOME/Workspace/Dev/Projects/mortworks/workflow-tools" ]]; then
+  export PROJECTS="$HOME/Workspace/Dev/Projects/mortworks"
+  export TOOLS_DIR="$PROJECTS/workflow-tools"
 else
   export PROJECTS="$HOME/mortworks"
-  export TOOLS_DIR="$HOME/mortworks/workflow-tools"
+  export TOOLS_DIR="$PROJECTS/workflow-tools"
 fi
 
-# Add workflow-tools binaries to PATH (if present)
 if [[ -d "$TOOLS_DIR/hugo-tools/bin" ]]; then
   export PATH="$TOOLS_DIR/hugo-tools/bin:$PATH"
 fi
